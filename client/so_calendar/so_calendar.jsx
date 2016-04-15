4bf3c3ff070797aa8c2ac8dd864e11cd254aa09a
Eric Bendall <Eric.Bendall@am.sony.com>
2016-04-14 22:58:31 -0400

    Populate grid with some data

--- client/so_calendar/so_calendar.jsx
+++ client/so_calendar/so_calendar.jsx
@@ -2,8 +2,48 @@ var React = require('react');
 
 var Day = require('./day/day.jsx');
 
+var createMonth = function(monthName, numDays, weekStart) {
+  var month = [[]];
+  var curWeekIx = 0;
+
+  var gridSize = (numDays + weekStart) + (7 - ((numDays + weekStart)%7));
+  for (var ix = 0; ix < gridSize; ix ++) {
+    if (month[curWeekIx].length === 7) {
+      curWeekIx++;
+      month.push([]);
+    }
+
+    var day = {id: ix};
+    if (ix < weekStart || ix >= weekStart + numDays){
+      day.label = 'Blank';
+    } else {
+      day.label = monthName + ' ' + (ix + 1 - weekStart);
+    }
+    month[curWeekIx].push(day);
+  }
+
+  return month;
+};
+
+var DayOfWeek = [
+  "Monday",
+  "Tueday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday"
+];
 
 var SoCalendar = React.createClass({
+
+  getDefaultProps: function(){
+    return {
+      month: createMonth('March', 31, 1),
+      calendarIdx: 15,
+      lastIdx: 14
+    };
+  },
 
   renderDay: function(dayOfWeek, position) {
     return <span className={position + ' dayCell'}>
@@ -11,15 +51,25 @@ var SoCalendar = React.createClass({
     </span>;
   },
 
+  renderDay2: function(calendarIdx, position) {
+    var weekIdx = Math.floor(calendarIdx / 7);
+    var dayIdx = calendarIdx % 7;
+    var label = DayOfWeek[dayIdx] + ': ' + this.props.month[weekIdx][dayIdx].label;
+    var key = this.props.month[weekIdx][dayIdx].id;
+    return <span key={key} className={position + ' dayCell'}>
+      <Day dayOfWeek={label} />
+    </span>;
+  },
+
   render : function(){
 
     return <div className="so-calendar">
       <div className="calendar">
-          {this.renderDay("Friday", "top center")}
-          {this.renderDay("Saturday", "middle left")}
-          {this.renderDay("Sunday", "middle center")}
-          {this.renderDay("Monday", "middle right")}
-          {this.renderDay("Tuesday", "bottom center")}
+          {this.renderDay2(this.props.calendarIdx - 7, "top center")}
+          {this.renderDay2(this.props.calendarIdx - 1, "middle left")}
+          {this.renderDay2(this.props.calendarIdx, "middle center")}
+          {this.renderDay2(this.props.calendarIdx + 1 , "middle right")}
+          {this.renderDay2(this.props.calendarIdx + 7, "bottom center")}
       </div>
     </div>
   },
