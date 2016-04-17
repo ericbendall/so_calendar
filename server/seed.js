fe7c03813e55fac809db5ccd9b994299c89d34e3
Eric Bendall <Eric.Bendall@am.sony.com>
2016-04-17 15:49:05 -0400

    updat seed function to wipe database before seeding

--- server/seed.js
+++ server/seed.js
@@ -13,25 +13,29 @@ var seed = function() {
       var db = mongoose.connection;
       db.on('error', reject);
       db.once('open', function() {
-        newCalendar = new Calendar({
-          name : "Default Seeded Calendar",
-          forId: "t",
-          creatorId: "e",
-          year: 2016,
-          dayNotes : []
-        });
 
-        var notes = [];
-        for (var ix = 0; ix < 365; ix++) {
-          notes.push({
-            note: "This is note number " + ix,
-            nsfw: (ix % 3) === 0
+        // reset all calendars
+      Calendar.remove()
+        .then( function() {
+          newCalendar = new Calendar({
+            name : 'Default Seeded Calendar',
+            forId: 't',
+            creatorId: 'e',
+            year: 2016,
+            dayNotes : []
           });
-        }
 
-        newCalendar.dayNotes = notes;
-        newCalendar.save();
-        resolve();
+          var notes = [];
+          for (var ix = 0; ix < 365; ix++) {
+            notes.push({
+              note: 'This is note number ' + ix + '. This is about the length as a love note should be. Maybe a bit more.',
+              nsfw: (ix % 3) === 0
+            });
+          }
+
+          newCalendar.dayNotes = notes;
+          newCalendar.save().then(resolve, reject);
+        }, reject)
       });
     } catch (e){
       reject(e);
